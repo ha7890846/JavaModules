a64334cde40330aa51b172222be85a3ad3a8d2d8
ha7890846 <ha7890846@gmail.com>
2024-05-25 23:03:15 +0530

    Pushing first program

---
+++ Basic/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    static int fib(int num){
+        // base Case
+        if(num == 0|| num == 1){
+            return num;
+        }
+        int ans = (fib(num-1)+fib(num-2));
+        return ans;
+    }
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the term: ");
+        num = sc.nextInt();
+        System.out.println("Printing the Fibonacci Series: ");
+        int i = 0;
+        while(i<num){
+            System.out.println(fib(i));
+            i++;
+        }
+    }
+}
