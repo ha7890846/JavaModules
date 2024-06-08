fd0221719afcc1031aecbd4c30461cbaaef88f57
ha7890846 <ha7890846@gmail.com>
2024-06-08 09:05:10 +0530

    store project

---
+++ OOPs/RealWorld/src/App.java
@@ -0,0 +1,8 @@
+import files.store;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        store obj = new store();
+        obj.calling();
+    }
+}
---
+++ OOPs/RealWorld/src/files/store.java
@@ -0,0 +1,7 @@
+package files;
+
+public class store {
+    public void calling(){
+        System.out.println("hello world");
+    }
+}
