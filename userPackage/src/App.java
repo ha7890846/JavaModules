73c8c55171d6db0740d9f6b741cdecdc1d6de828
ha7890846 <ha7890846@gmail.com>
2024-07-12 09:59:43 +0530

    user defined Package

---
+++ userPackage/src/App.java
@@ -0,0 +1,9 @@
+import mypack.Student;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Student s = new Student();
+        s.run();
+    }
+}
---
+++ userPackage/src/mypack/Student.java
@@ -0,0 +1,6 @@
+package mypack;
+public class Student {
+    public void run(){
+        System.out.println("Running dev.");
+    }
+}
